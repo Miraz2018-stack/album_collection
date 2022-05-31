0c495ffe5d9d00d0fdf041af4b8d7c01c4c9ce2c
unknown <you@example.com>
2022-05-31 16:34:34 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './header.css';
+
+
+export default () => (
+    <header className="header">
+        <a href="https://saigowthamr.github.io/Itunes-Top-100-albums/" style={{
+            textDecoration: "none",
+            color: "white", borderBottom: "1px solid red"
+        }}>Top 100 Albums from Itunes</a>
+    </header>
+)
